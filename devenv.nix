b146fdbef288ee370f952cba4be39e31a804e569
Durable Programming, LLC Team <djberube@durableprogramming.com>
2025-11-21 12:40:13 -0500

    isolate CARGO_HOME

--- devenv.nix
+++ devenv.nix
@@ -7,11 +7,11 @@
   languages.rust = {
     enable = true;
     channel = "stable";
-    components = [ "rustc" "cargo" "rust-std" ];
+    components = [ "rustc" "cargo" "rust-std" "clippy" "rustfmt" ];
     targets = [ "x86_64-unknown-linux-musl" "aarch64-unknown-linux-musl" ];
   };
 
-
+  env.CARGO_HOME = "${config.devenv.root}/.devenv/cargo";
   env.LLVM_SYS_180_PREFIX = "${pkgs.llvm_18.dev}";
 
   enterShell = ''
